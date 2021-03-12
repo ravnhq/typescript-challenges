8fcad9eff2167058dd75e0da54b09ac5dbb2f634
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-12 16:07:28 -0500

    exercise 108

--- questions/108-medium-trim/template.ts
+++ questions/108-medium-trim/template.ts
@@ -1,1 +1,6 @@
-export type Trim<S extends string> = any
+export type Trim<S extends string> = 
+  S extends `${' ' | '\n\t'}${infer R}` 
+  ? Trim<R> 
+  : S extends `${infer R2}${' ' | '\t'}` 
+    ? Trim<R2>
+    : S 
