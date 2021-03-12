a16b377259450c999253cd5755a5e4b56b2ab572
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-12 18:31:50 -0500

    exercise 610

--- questions/610-medium-camelcase/template.ts
+++ questions/610-medium-camelcase/template.ts
@@ -1,1 +1,5 @@
-export type CamelCase<S> = any
+export type CamelCase<S extends string> = S extends `${infer START}${'-'}${infer U}${infer END}`
+  ? `${'-'}${U}` extends `${'-'}${Uppercase<U>}`
+    ? `${START}${'-'}${CamelCase<`${Uppercase<U>}${END}`>}`
+    : `${START}${CamelCase<`${Uppercase<U>}${END}`>}`
+  : S
