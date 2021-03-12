117b7d84c50583dd5195dfb8e93080ae934bce75
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-12 16:30:21 -0500

    exercise 119

--- questions/119-medium-replaceall/template.ts
+++ questions/119-medium-replaceall/template.ts
@@ -2,4 +2,8 @@ export type ReplaceAll<
   S extends string,
   From extends string,
   To extends string
-> = any
+> = From extends '' 
+  ? S 
+  : S extends `${infer START}${From}${infer END}` 
+    ? `${START}${To}${ReplaceAll<END, From, To>}`
+    : S
