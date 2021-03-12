23445e3c648252adc76d75b579357be97e3dbee9
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-12 17:32:22 -0500

    exercise 459

--- questions/459-medium-flatten/template.ts
+++ questions/459-medium-flatten/template.ts
@@ -1,1 +1,5 @@
-export type Flatten = any
+export type Flatten<T extends any[]> = T extends [infer I, ...infer R] 
+  ? I extends any[] 
+    ? [...Flatten<I>, ...Flatten<R>]
+    : [I, ...Flatten<R>] 
+  : T
