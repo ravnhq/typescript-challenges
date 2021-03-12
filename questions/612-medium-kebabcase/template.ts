a657d06b391eb641e0f9388bf391be86786a463e
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-12 18:59:33 -0500

    exercise 612

--- questions/612-medium-kebabcase/template.ts
+++ questions/612-medium-kebabcase/template.ts
@@ -1,1 +1,5 @@
-export type KebabCase<S> = any
+export type KebabCase<S extends string> = S extends `${infer START}${infer MID}${infer END}` 
+  ? MID extends `${Lowercase<MID>}`
+    ? `${Lowercase<START>}${KebabCase<`${MID}${END}`>}`
+    : `${Lowercase<START>}-${KebabCase<`${Lowercase<MID>}${END}`>}`
+  : S
