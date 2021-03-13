19cd2e921f92d1a29a362ddc596729570bced546
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-13 13:54:47 -0500

    exercise 298

--- questions/298-medium-length-of-string/template.ts
+++ questions/298-medium-length-of-string/template.ts
@@ -1,1 +1,4 @@
-export type LengthOfString<S extends string> = any
+export type LengthOfString<S extends string, T extends string[] = []> = 
+  S extends `${infer START}${infer END}`
+    ? LengthOfString<END, [...T, START]> 
+    : T['length']
