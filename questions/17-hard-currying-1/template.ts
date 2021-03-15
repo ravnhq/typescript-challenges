75e05b778d3412419d175fc139261439c5713dcb
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-15 11:52:50 -0500

    exercise 17

--- questions/17-hard-currying-1/template.ts
+++ questions/17-hard-currying-1/template.ts
@@ -1,1 +1,9 @@
-export declare function Currying(fn: any): any
+type Head<T extends any[]> = T extends [any, ...any[]] ? T[0] : never
+type Tail<T extends any[]> = ((...t: T) => any) extends ((_:any, ...tail: infer TT) => any) ? TT : []
+type HasTail<T extends any[]> = T extends ([] | [any]) ? false : true
+
+type CurryV0<P extends any[], R> = (args0: Head<P>) => HasTail<P> extends true 
+  ? CurryV0<Tail<P>, R>
+  : R
+
+export declare function Currying<P extends any[], R>(f: (...fn: P) => R): CurryV0<P, R>
