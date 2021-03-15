fbe1b634af567c3c81d657ce92f68b37293a2682
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-15 15:43:31 -0500

    exercise 399

--- questions/399-hard-tuple-filter/template.ts
+++ questions/399-hard-tuple-filter/template.ts
@@ -1,1 +1,7 @@
-export type FilterOut<T extends any[], F> = any
+export type FilterOut<T extends any[], F> = T extends [infer T0, ...infer T1] 
+  ? [T0] extends [F]
+    ? FilterOut<T1, F>
+    : [T0, ...FilterOut<T1, F>]
+  : []
+
+type Filtered = FilterOut<[1, 2, null, 3], null> // [1, 2, 3]
