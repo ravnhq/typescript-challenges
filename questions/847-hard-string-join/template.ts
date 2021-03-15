f899b1cd374b70233a922ea5b1f43aacb65a5d83
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-15 17:17:34 -0500

    exercise 847

--- questions/847-hard-string-join/template.ts
+++ questions/847-hard-string-join/template.ts
@@ -1,1 +1,9 @@
-declare function join(delimiter: any): (...parts: any[]) => any
+type JoinType<T extends string, U extends string[]> = U extends [infer U0, ...infer U1]
+  ? U1 extends []
+    ? `${U[0]}` 
+    : U1 extends string[] 
+      ? `${U[0]}${T}${JoinType<T, U1>}` 
+      : never
+  : ''
+   
+declare function join<T extends string>(delimiter: T): <U extends string[]>(...parts: U) => JoinType<T, U>
