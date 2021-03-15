49bb7ad04c6a2fa3caa8f83057bbc7a7e68ead51
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-15 12:26:15 -0500

    exercise 114

--- questions/114-hard-camelcase/template.ts
+++ questions/114-hard-camelcase/template.ts
@@ -1,1 +1,3 @@
-export type CamelCase<S extends string> = any
+export type CamelCase<S extends string> = S extends `${infer S0}_${infer S2}`
+  ? `${Lowercase<S0>}${Capitalize<CamelCase<S2>>}`
+  : Lowercase<S>
