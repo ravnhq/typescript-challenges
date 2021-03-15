ec5b7337792232ac1ed73385c213f3770fb6a8dd
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-15 16:13:15 -0500

    exercise 545

--- questions/545-hard-printf/template.ts
+++ questions/545-hard-printf/template.ts
@@ -1,1 +1,10 @@
-export type Format<T extends string> = any
+export type Formats = {
+  s : string
+  d : number
+}
+
+export type Format<T extends string> = T extends `${infer S0}%${infer S1}${infer S2}`
+  ? S1 extends keyof Formats
+    ? (s1: Formats[S1]) => Format<S2>
+    : Format<S2>
+  : string
