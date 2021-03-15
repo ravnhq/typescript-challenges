81602e2431fdb7f06938a657ef4bd27b02639b36
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-15 11:57:55 -0500

    exercise 112

--- questions/112-hard-capitalizewords/template.ts
+++ questions/112-hard-capitalizewords/template.ts
@@ -1,1 +1,7 @@
-export type CapitalizeWords<S extends string> = any
+export type CapitalizeWord<S extends string> = S extends `${infer S0}${infer S1}${infer S2}`
+  ? S1 extends ' ' | ',' | '.'
+    ? `${S0}${S1}${CapitalizeWord<Capitalize<`${S2}`>>}`
+    : `${S0}${S1}${CapitalizeWord<`${S2}`>}`
+  : S
+
+export type CapitalizeWords<S extends string> = Capitalize<CapitalizeWord<S>>
