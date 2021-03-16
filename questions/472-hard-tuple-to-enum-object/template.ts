8b00f7e4314a9f97ff0cc9d870c5c459d21a2e53
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-16 16:54:36 -0500

    exercise 472

--- questions/472-hard-tuple-to-enum-object/template.ts
+++ questions/472-hard-tuple-to-enum-object/template.ts
@@ -1,1 +1,9 @@
-export type Enum<T extends readonly string[], N extends boolean = false> = any
+type Length<T, K, R extends any[] = []> = T extends readonly [infer T0, ...infer T1]
+  ? T0 extends K
+    ? R
+    : Length<T1, K, [K, ...R]>
+  : []
+
+export type Enum<T extends readonly string[], N extends boolean = false> = N extends false 
+  ? { readonly [K in T[number] as `${Capitalize<K>}`]: K }
+  : { readonly [K in T[number] as `${Capitalize<K>}`] : Length<T, K>['length']}
