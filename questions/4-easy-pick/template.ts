466ee54b305656df9decec39ce91cc1bf45f21bb
Patrick Lazo <patrickdz96@gmail.com>
2021-08-20 18:48:00 -0500

    added comment 4-easy-pick

--- questions/4-easy-pick/template.ts
+++ questions/4-easy-pick/template.ts
@@ -1,3 +1,6 @@
+// Notice that we use K extends keyof T to make sure that we are only passing properties belonging to the type/interface.
 export type MyPick<T, K extends keyof T> = {
   [key in K]: T[key]
 }
+
+type a = Partial<any>;
