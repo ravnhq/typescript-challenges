c07010eadaa245596c9c5e4a0723a93883e46f62
Patrick <patrickdz96@gmail.com>
2021-08-22 18:33:10 -0500

    solved 11-easy-tuple-to-object

--- questions/11-easy-tuple-to-object/template.ts
+++ questions/11-easy-tuple-to-object/template.ts
@@ -1,1 +1,4 @@
-export type TupleToObject<T extends readonly any[]> = any
+//I think number is neccesary to iterate the array and with ':' the array is converted to object setting the value as k
+export type TupleToObject<T extends readonly string[]> = {
+    [k in T[number]]: k
+}
