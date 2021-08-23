fb3469e4ad3fab76ce9df70000bc6105e9b967bc
Patrick <patrickdz96@gmail.com>
2021-08-22 20:08:28 -0500

    solved 189-easy-awaited

--- questions/189-easy-awaited/template.ts
+++ questions/189-easy-awaited/template.ts
@@ -1,1 +1,2 @@
-export type Awaited = any
+// Using the infer keyword is like telling Typescript, “I want to take whatever you infer to be at this position and assign it to a new type variable.”
+export type Awaited<T> = T extends Promise<infer R> ? R : never;
