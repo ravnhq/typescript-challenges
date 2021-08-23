5d51d03cc7e301dc0d6dff95b10e279aefa561ee
Patrick <patrickdz96@gmail.com>
2021-08-23 17:09:15 -0500

    solved 106-medium-trimleft

--- questions/106-medium-trimleft/template.ts
+++ questions/106-medium-trimleft/template.ts
@@ -1,1 +1,3 @@
-export type TrimLeft<S extends string> = any
+//export type TrimLeft<S extends string> = any
+type Space = ' ' | '\n' | '\t'
+export type TrimLeft<S extends string> = S extends `${Space}${infer Rest}` ? TrimLeft<Rest> : S;
