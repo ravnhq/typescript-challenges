d1c7458c7584b8ba759a1ae64b5111da76985709
Patrick <patrickdz96@gmail.com>
2021-08-23 21:20:27 -0500

    solved 191-medium-append-argument

--- questions/191-medium-append-argument/template.ts
+++ questions/191-medium-append-argument/template.ts
@@ -1,1 +1,2 @@
-export type AppendArgument<Fn, A> = any
+// https://www.typescriptlang.org/docs/handbook/advanced-types.html#type-inference-in-conditional-types
+export type AppendArgument<Fn, A> = Fn extends (...args: infer P) => infer R ? (...args2: [...P, A]) => R : never
