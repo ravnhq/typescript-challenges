9da882487cdc926ee4de878d951bea8df712907e
Patrick <patrickdz96@gmail.com>
2021-08-24 11:42:28 -0500

    solved 610-medium-camelcase

--- questions/610-medium-camelcase/template.ts
+++ questions/610-medium-camelcase/template.ts
@@ -1,1 +1,6 @@
-export type CamelCase<S> = any
+export type CamelCase<S extends string> =
+    S extends `${infer Head}-${infer Letter}${infer Rest}`
+    ? Letter extends Capitalize<Letter>
+    ? `${Head}-${CamelCase<`${Letter}${Rest}`>}`
+    : `${Head}${CamelCase<`${Capitalize<Letter>}${Rest}`>}`
+    : S;
