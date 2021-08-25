5dfb013afdde34caf1358a369b348e383e0d76e1
Patrick <patrickdz96@gmail.com>
2021-08-24 21:12:02 -0500

    solved 296-medium-permutation

--- questions/296-medium-permutation/template.ts
+++ questions/296-medium-permutation/template.ts
@@ -1,1 +1,8 @@
-export type Permutation<T> = any
+//[T] extends [never] to stop it being distributive.
+//K extends K to apply N! tuples 
+export type Permutation<T, K = T> =
+    [T] extends [never]
+    ? []
+    : K extends K
+    ? [K, ...Permutation<Exclude<T, K>>]
+    : never;
