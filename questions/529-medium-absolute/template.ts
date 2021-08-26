0242a9ff0302072b329b08233361eb7a53f4bd21
Alexander <alexander.pinto@ucsp.edu.pe>
2021-08-25 20:19:56 -0500

    solved: medium absolute

--- questions/529-medium-absolute/template.ts
+++ questions/529-medium-absolute/template.ts
@@ -1,1 +1,8 @@
-export type Absolute<T extends number | string | bigint> = any
+export type Absolute<T extends number | string | bigint>
+  = T extends number | bigint
+    ? Absolute<`${T}`>
+    : T extends `${'-'}${infer num}`
+      ? `${num}`
+      : T extends `${infer num}`
+        ? `${num}`
+        : T
