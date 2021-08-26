dc3e75af87c643d20aaa58f612a597e41421faaa
csagastegui59 <csagastegui59@gmail.com>
2021-08-26 00:55:13 -0500

    solved medium-capitalize

--- questions/116-medium-replace/template.ts
+++ questions/116-medium-replace/template.ts
@@ -1,5 +1,5 @@
-export type Replace<
-  S extends string,
-  From extends string,
-  To extends string
-> = any
+export type Replace<S extends string, From extends string, To extends string> = S extends `${infer F}${From}${infer E}` 
+? From extends '' 
+? S 
+: `${F}${To}${E}` 
+: S
