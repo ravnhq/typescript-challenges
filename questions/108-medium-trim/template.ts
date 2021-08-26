a067ecaca0ef610fad7e22aed0204a827b17d74c
Kevin Cotrina <kcotrinam92@gmail.com>
2021-08-25 19:19:00 -0500

    Add solution for challenge 108-medium-trim

--- questions/108-medium-trim/template.ts
+++ questions/108-medium-trim/template.ts
@@ -1,1 +1,7 @@
-export type Trim<S extends string> = any
+type exclude = ' ' | '\n' | '\t'
+
+export type Trim<S extends string> = S extends `${exclude}${infer restL}`
+  ? Trim<restL>
+  : S extends `${infer restR}${exclude}`
+  ? Trim<restR>
+  : S
