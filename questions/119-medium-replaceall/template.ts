040bd460863c75162038a0d924fe6a6a7ef2ce28
csagastegui59 <csagastegui59@gmail.com>
2021-08-26 00:57:12 -0500

    solved medium-replaceAll

--- questions/119-medium-replaceall/template.ts
+++ questions/119-medium-replaceall/template.ts
@@ -1,5 +1,5 @@
-export type ReplaceAll<
-  S extends string,
-  From extends string,
-  To extends string
-> = any
+export type ReplaceAll<S extends string, From extends string, To extends string> = From extends '' 
+? S 
+: S extends `${infer first}${From}${infer rest}` 
+? `${first}${To}${ReplaceAll<rest, From, To>}` 
+: S
