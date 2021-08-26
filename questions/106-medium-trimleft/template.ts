fa96ef237beef685a1230bf40afd49fa3ccb07ae
Kevin Cotrina <kcotrinam92@gmail.com>
2021-08-25 19:18:37 -0500

    Add solution for challenge 106-medium-trimleft

--- questions/106-medium-trimleft/template.ts
+++ questions/106-medium-trimleft/template.ts
@@ -1,1 +1,6 @@
-export type TrimLeft<S extends string> = any
+type exclude = ' ' | '\n' | '\t'
+export type TrimLeft<S extends string> = S extends `${exclude}${infer rest}`
+  ? TrimLeft<rest>
+  : S
+
+type trimed = TrimLeft<'  Hello World  '> // expected to be 'Hello World  '
