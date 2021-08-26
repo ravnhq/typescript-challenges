6f710e57e6e70c365fdd5de56f596c3bfacd71bd
userlifex <eholguinruiz@gmail.com>
2021-08-25 22:03:06 -0500

    camelcase

--- questions/610-medium-camelcase/template.ts
+++ questions/610-medium-camelcase/template.ts
@@ -1,1 +1,6 @@
-export type CamelCase<S> = any
+export type CamelCase<S extends string> = S extends `${infer A}-${infer B}`
+  ? B extends Capitalize<B>
+    ? `${A}-${CamelCase<B>}`
+    : `${A}${CamelCase<Capitalize<B>>}`
+  : S
+
