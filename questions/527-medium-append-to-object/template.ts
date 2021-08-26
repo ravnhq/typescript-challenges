488475ab3e9ae42f86cbd97a5ab30093a0577820
Alexander <alexander.pinto@ucsp.edu.pe>
2021-08-25 20:14:15 -0500

    solved: medium append to object

--- questions/527-medium-append-to-object/template.ts
+++ questions/527-medium-append-to-object/template.ts
@@ -1,1 +1,10 @@
-export type AppendToObject<T, U, V> = any
+type Merge<T> = T extends object
+  ? { [K in keyof T]: T[K] }
+  : T
+
+export type AppendToObject<T extends {}, U extends string, V>
+  = Merge<
+    { [Property in keyof T] : T[Property] }
+      &
+    { [Property in U] : V}
+    > ;
