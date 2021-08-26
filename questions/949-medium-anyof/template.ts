abb66a2a8410a74ce33eebdc4b3d2dd520338c40
Alexander <alexander.pinto@ucsp.edu.pe>
2021-08-25 20:34:34 -0500

    solved: medium anyof

--- questions/949-medium-anyof/template.ts
+++ questions/949-medium-anyof/template.ts
@@ -1,1 +1,14 @@
-export type AnyOf<T extends readonly any[]> = any
+type Falsy<T> = T extends false | 0 | '' | []
+  ? true
+  : T extends { [x: string ]: any }
+    ? [keyof T] extends [never]
+      ? true
+      : false
+    : false;
+
+export type AnyOf<T extends readonly any[]>
+  = T extends [infer first, ...infer rest]
+    ? Falsy<first> extends true // Is falsy?
+      ? AnyOf <rest> // continue search
+      : true // finish search
+    : false
