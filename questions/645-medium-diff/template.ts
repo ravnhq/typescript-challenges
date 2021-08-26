7cd7343cf8ea8342ac13526ade72be587705f25c
Alexander <alexander.pinto@ucsp.edu.pe>
2021-08-25 20:31:58 -0500

    solved: medium diff

--- questions/645-medium-diff/template.ts
+++ questions/645-medium-diff/template.ts
@@ -1,1 +1,10 @@
-export type Diff<O, O1> = any
+export type Diff<O extends object, O1 extends object> = {
+  [property in Prop2Union<O, O1>]: property extends keyof O
+    ? O[property]
+    : property extends keyof O1
+      ? O1[property]
+      : never
+}
+
+type Prop2Union<T extends object, U extends object>
+  = Exclude<keyof T,keyof U> | Exclude<keyof U, keyof T>
