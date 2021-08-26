ccd9e4359f02d92dc1941cbc6a28b8d71410622a
Alexander <alexander.pinto@ucsp.edu.pe>
2021-08-25 19:40:19 -0500

    solved: medium promiseall

--- questions/20-medium-promise-all/template.ts
+++ questions/20-medium-promise-all/template.ts
@@ -1,1 +1,9 @@
-export declare function PromiseAll(values: any): any
+//Compiler problem es5 -> es2015 solved
+
+export declare function PromiseAll<T extends unknown[]>(
+  values: readonly [...T],
+): Promise<
+  {
+    [key in keyof T]: T[key] extends Promise<infer U> ? U : T[key]
+  }
+>
