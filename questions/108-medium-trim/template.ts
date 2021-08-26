0c357fa0dc64583790c1632d03fe6ffb35e5d030
userlifex <eholguinruiz@gmail.com>
2021-08-25 21:58:26 -0500

    solve: trim

--- questions/108-medium-trim/template.ts
+++ questions/108-medium-trim/template.ts
@@ -1,1 +1,8 @@
-export type Trim<S extends string> = any
+type TrimChar = ' ' | '\n' | '\t'
+
+export type Trim<S> = S extends `${TrimChar}${infer R}`
+  ? Trim<R>
+  : S extends `${infer Q}${TrimChar}`
+  ? Trim<Q>
+  : S
+
