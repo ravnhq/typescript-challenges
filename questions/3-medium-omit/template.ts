dbca755c92090d619f52cc3dfd9358dc3de78396
userlifex <eholguinruiz@gmail.com>
2021-08-25 21:54:35 -0500

    solve: 3 medium ommit

--- questions/3-medium-omit/template.ts
+++ questions/3-medium-omit/template.ts
@@ -1,1 +1,16 @@
-export type MyOmit<T, K> = any
+export type MyOmit<T, K> = {
+  [P in keyof T as P extends K ? never : P]: T[P]
+}
+
+interface hel {
+  name: string
+  last: string
+  age: number
+}
+
+type ro<T, U extends keyof T > = T &
+  {
+    readonly [P in U]: T[P]
+  }
+
+type test = ro<hel, 'name'>
