692d0904d4a5a870bb879cb66dc81e4570f4d826
Alexander <alexander.pinto@ucsp.edu.pe>
2021-08-25 20:24:36 -0500

    solved: medium merge

--- questions/599-medium-merge/template.ts
+++ questions/599-medium-merge/template.ts
@@ -1,1 +1,7 @@
-export type Merge<F, S> = any
+export type Merge<F, S> = {
+  [property in keyof F | keyof S] : property extends keyof S
+    ? S[property]
+    : property extends keyof F
+      ? F[property]
+      : never
+}
