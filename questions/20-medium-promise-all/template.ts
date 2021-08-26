88ef6992362c063473dbef9b05984e8604892272
userlifex <eholguinruiz@gmail.com>
2021-08-25 21:57:14 -0500

    solve: 20 promise-all

--- questions/20-medium-promise-all/template.ts
+++ questions/20-medium-promise-all/template.ts
@@ -1,1 +1,5 @@
-export declare function PromiseAll(values: any): any
+export declare function PromiseAll<T extends unknown[]>(
+  args: readonly [...T],
+): Promise<{ [P in keyof T]: T[P] extends Promise<infer X> ? X : T[P] }>
+
+//must change es6 compiler to work
