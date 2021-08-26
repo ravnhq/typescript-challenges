c5a02320179d71644e8b9b7d7d5d14baf5c0c459
csagastegui59 <csagastegui59@gmail.com>
2021-08-26 00:45:22 -0500

    solved medium-trim

--- questions/108-medium-trim/template.ts
+++ questions/108-medium-trim/template.ts
@@ -1,1 +1,5 @@
-export type Trim<S extends string> = any
+export type Trim<S extends string> = S extends `${' ' | '\t' | '\n'}${infer R}` 
+? Trim<R> 
+: S extends `${infer R}${' ' | '\t' | '\n'}` 
+? Trim<R> 
+: S
