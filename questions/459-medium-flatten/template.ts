39195211e8dbf64b12aa0083b260a85b693c6f51
Alexander <alexander.pinto@ucsp.edu.pe>
2021-08-25 20:11:03 -0500

    solved: medium flatten

--- questions/459-medium-flatten/template.ts
+++ questions/459-medium-flatten/template.ts
@@ -1,1 +1,6 @@
-export type Flatten = any
+export type Flatten<T extends unknown[]>
+  = T extends [infer Left, ...infer Right]
+    ? ( Left extends unknown[]
+      ? [...Flatten<Left>, ...Flatten<Right>]
+      : [Left, ...Flatten<Right>])
+    : T
