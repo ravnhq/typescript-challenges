13c120d518afb7c7e2f1dcbaa9e0f003531736fe
Alexander <alexander.pinto@ucsp.edu.pe>
2021-08-25 20:28:07 -0500

    solved: medium camel case

--- questions/610-medium-camelcase/template.ts
+++ questions/610-medium-camelcase/template.ts
@@ -1,1 +1,6 @@
-export type CamelCase<S> = any
+export type CamelCase<S>
+  = S extends `${infer before}${'-'}${infer rest}`
+    ? rest extends Capitalize<rest> // Is a Uppercase or symbol?
+      ? `${before}-${CamelCase<rest>}` // is symbol or upper -> continue
+      : `${before}${CamelCase<Capitalize<rest>>}` // is lower -> make upper n continue
+    : S
