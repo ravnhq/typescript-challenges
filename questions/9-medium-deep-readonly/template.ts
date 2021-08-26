d2797ca6a7583c924775f10edf41b9e650500f65
Alexander <alexander.pinto@ucsp.edu.pe>
2021-08-25 19:26:53 -0500

    solved: medium deep read only

--- questions/9-medium-deep-readonly/template.ts
+++ questions/9-medium-deep-readonly/template.ts
@@ -1,1 +1,4 @@
-export type DeepReadonly<T> = any
+export type DeepReadonly<T> = {
+  readonly [property in keyof T]: keyof T[property] extends undefined
+    ? T[property] : DeepReadonly<T[property]>
+}
