5ad19b915ed26d45a0f0f398dfb226c806c67b03
Alexander <alexander.pinto@ucsp.edu.pe>
2021-08-25 19:57:11 -0500

    solved: medium replace all

--- questions/119-medium-replaceall/template.ts
+++ questions/119-medium-replaceall/template.ts
@@ -2,4 +2,9 @@ export type ReplaceAll<
   S extends string,
   From extends string,
   To extends string
-> = any
+>
+  = S extends `${infer Before}${From}${infer After}`
+    ? From extends ''
+      ? S
+      : `${Before}${To}${ReplaceAll<After, From, To>}`
+    : S
