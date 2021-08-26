045604491fa2d3ec861f9dc5f608356714d7c93c
userlifex <eholguinruiz@gmail.com>
2021-08-25 21:59:10 -0500

    solve: replace

--- questions/116-medium-replace/template.ts
+++ questions/116-medium-replace/template.ts
@@ -2,4 +2,5 @@ export type Replace<
   S extends string,
   From extends string,
   To extends string
-> = any
+> = From extends '' ? S 
+  : S extends `${infer X}${From}${infer Y}` ? `${X}${To}${Y}` : S
