f1ecd46b4bfa9c2607ef8236241ccfed328a32f1
Alexander <alexander.pinto@ucsp.edu.pe>
2021-08-25 20:29:58 -0500

    solved: medium kebab case

--- questions/612-medium-kebabcase/template.ts
+++ questions/612-medium-kebabcase/template.ts
@@ -1,1 +1,6 @@
-export type KebabCase<S> = any
+export type KebabCase<S extends string>
+  = S extends `${infer before}${infer after}`
+    ? after extends Uncapitalize<after> // is lowercase or symbol?
+      ? `${Uncapitalize<before>}${KebabCase<after>}` // true lowercase
+      : `${Uncapitalize<before>}-${KebabCase<`${Uncapitalize<after>}`>}` // false lowercase
+    : S;
