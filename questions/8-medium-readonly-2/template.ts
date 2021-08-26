34293520485084517d25b84b467fc4a64dec6f96
Alexander <alexander.pinto@ucsp.edu.pe>
2021-08-25 19:21:11 -0500

    solved: medium readonly2

--- questions/8-medium-readonly-2/template.ts
+++ questions/8-medium-readonly-2/template.ts
@@ -1,1 +1,9 @@
-export type MyReadonly2<T, K> = any
+type MyExclude<T, U> = T extends U ? never : T
+
+export type MyReadonly2<T, K extends keyof T = keyof T> = {
+   readonly [P in K]: T[P]
+  }
+  &
+  {
+     [Key in MyExclude<keyof T, K>]: T[Key]
+  }
