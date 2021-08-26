5e032ca94da46bf79ecdba4422da63db8b0a63a0
Alexander <alexander.pinto@ucsp.edu.pe>
2021-08-25 20:09:27 -0500

    solved: medium length of string

--- questions/298-medium-length-of-string/template.ts
+++ questions/298-medium-length-of-string/template.ts
@@ -1,1 +1,15 @@
-export type LengthOfString<S extends string> = any
+export type String2Tuple<
+  S extends string,
+  C extends unknown[] = []
+>
+  = S extends `${infer char}${infer rest}`
+      ? [...C,...String2Tuple<rest, [char]>]
+      : C;
+
+export type Length<T extends array> = T extends array ? T['length'] : 0
+
+export type LengthOfString<T extends string> = Length<String2Tuple<T>>
+
+interface array {
+  length: number
+}
