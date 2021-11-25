acad8641ed9ab22e4bfed815e9fb806a90944cfb
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-11-25 16:03:26 -0500

    feat: solve challenge 20 medium promise all

--- questions/20-medium-promise-all/template.ts
+++ questions/20-medium-promise-all/template.ts
@@ -1,1 +1,5 @@
-export declare function PromiseAll(values: any): any
+import { Awaited } from "../189-easy-awaited/template";
+
+export declare function PromiseAll<T extends any[]>(values: readonly [...T]): Promise<{
+    [Idx in keyof T]: Awaited<T[Idx]>
+}>
