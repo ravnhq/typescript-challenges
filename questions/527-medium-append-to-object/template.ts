4152070a5084de434e94022426a3768ec4fd5443
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-12-06 21:38:04 -0500

    feat: solve challenge 527 medium appent to object

--- questions/527-medium-append-to-object/template.ts
+++ questions/527-medium-append-to-object/template.ts
@@ -1,1 +1,4 @@
-export type AppendToObject<T, U, V> = any
+type MapObject<O> = { [Key in keyof O]: O[Key] }
+
+export type AppendToObject<T extends { [x: string]: unknown }, U extends string, V> =
+    MapObject<T & { [Key in U]: V }>
