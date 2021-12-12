5d5230913a3ca8ad3409ea0cda3ffd51a750c517
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-12-12 13:44:47 -0500

    feat: solve challenge 949 medium anyof

--- questions/949-medium-anyof/template.ts
+++ questions/949-medium-anyof/template.ts
@@ -1,1 +1,4 @@
-export type AnyOf<T extends readonly any[]> = any
+type FalsyValues = 0 | '' | false | readonly [] | { [k: string]: never } | null | undefined
+
+export type AnyOf<T extends readonly any[]> =
+    T extends [infer H, ...infer R] ? H extends FalsyValues ? AnyOf<R> : true : false
