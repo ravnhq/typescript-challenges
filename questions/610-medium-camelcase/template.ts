35795b44fd7e86b7c0e63c4d6de071ee68869e21
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-12-12 12:54:30 -0500

    feat: solve challenge 619 medium camelcase

--- questions/610-medium-camelcase/template.ts
+++ questions/610-medium-camelcase/template.ts
@@ -1,1 +1,4 @@
-export type CamelCase<S> = any
+type Transform<H extends string, R extends string> = `${H}${R extends Capitalize<R> ? `-${CamelCase<R>}` : CamelCase<Capitalize<R>>}`
+
+export type CamelCase<S extends string> = S extends `${infer H}-${infer R}` ? Transform<H, R> : S
+
