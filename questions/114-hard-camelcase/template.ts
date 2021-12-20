ad2077f459e9a20f456c7e2a2767e6c5866e5f83
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-12-20 10:12:03 -0500

    feat: solve challenge 114 hard camelCase

--- questions/114-hard-camelcase/template.ts
+++ questions/114-hard-camelcase/template.ts
@@ -1,1 +1,5 @@
-export type CamelCase<S extends string> = any
+type PascalCase<S extends string> =
+    S extends `${infer W}_${infer R}` ? `${Capitalize<Lowercase<W>>}${PascalCase<R>}` : Capitalize<Lowercase<S>>
+
+export type CamelCase<S extends string> = Uncapitalize<PascalCase<S>>
+
