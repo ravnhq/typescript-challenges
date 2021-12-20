178e2209fa7411a67218b0107b45771583def2fe
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-12-20 11:08:24 -0500

    feat: solve challenge 300 hard string to number

--- questions/300-hard-string-to-number/template.ts
+++ questions/300-hard-string-to-number/template.ts
@@ -1,1 +1,8 @@
-export type ToNumber<S extends string> = any
+type Multiply10<T extends any[]> = [...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T]
+
+type DigitToNumber<T extends string | number, R extends any[] = []> =
+    `${T}` extends `${R['length']}` ? R : DigitToNumber<T, [any, ...R]>
+
+export type ToNumber<S extends string, R extends any[] = []> =
+    S extends `${infer H}${infer T}` ? ToNumber<T, [...Multiply10<R>, ...DigitToNumber<H>]> : R['length']
+
