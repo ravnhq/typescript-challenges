e74febd564a5680827971bd851a2b623ad1bebf5
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-12-20 10:04:10 -0500

    feat: solve challenge 112 hard capitalize words

--- questions/112-hard-capitalizewords/template.ts
+++ questions/112-hard-capitalizewords/template.ts
@@ -1,1 +1,5 @@
-export type CapitalizeWords<S extends string> = any
+type CapitalizeWithSep<S extends string, Sep extends string> =
+    S extends `${infer W}${Sep}${infer R}` ? `${Capitalize<W>}${Sep}${CapitalizeWithSep<R, Sep>}` : Capitalize<S>
+
+export type CapitalizeWords<S extends string> =
+    CapitalizeWithSep<CapitalizeWithSep<CapitalizeWithSep<S, ' '>, ','>, '.'>
