86cd5fbddc26adf7e5a7335f3254818df6cee7b0
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-12-20 11:59:57 -0500

    feat: solve challenge 545 hard printf

--- questions/545-hard-printf/template.ts
+++ questions/545-hard-printf/template.ts
@@ -1,1 +1,9 @@
-export type Format<T extends string> = any
+export type ControlsMap = {
+    s: string
+    d: number
+}
+
+export type Format<T extends string> =
+    T extends `${infer _}%${infer C}${infer R}`
+    ? C extends keyof ControlsMap ? (_: ControlsMap[C]) => Format<R> : string
+    : string
