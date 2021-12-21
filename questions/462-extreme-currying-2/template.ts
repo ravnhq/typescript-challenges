78fc52b793d862aeb1e8711fb60053058c087a40
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-12-21 11:57:12 -0500

    feat: solve challenge 462 extreme currying 2

--- questions/462-extreme-currying-2/template.ts
+++ questions/462-extreme-currying-2/template.ts
@@ -1,1 +1,7 @@
-export declare function DynamicParamsCurrying(fn: any): any
+type DynamicCurry<Args, Ret, D extends unknown[] = []> =
+    Args extends [infer H, ...infer T]
+    ? T extends [] ? (...args: [...D, H]) => Ret : ((...args: [...D, H]) => DynamicCurry<T, Ret>) & DynamicCurry<T, Ret, [...D, H]>
+    : () => Ret
+
+export declare function DynamicParamsCurrying<Args extends unknown[], Ret>(fn: (...args: Args) => Ret):
+    DynamicCurry<Args, Ret>
